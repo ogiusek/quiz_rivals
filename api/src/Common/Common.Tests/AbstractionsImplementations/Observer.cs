f02e8d896686abe88d4296ca77efaaf4ad9528c0
ogius <kowalewski.olgierd@gmail.com>
2024-10-15 19:36:30 +0200

    added res and observer tests and added Clock and AppWbeSocketMock

---
+++ api/src/Common/Common.Tests/AbstractionsImplementations/Observer.cs
@@ -0,0 +1,80 @@
+using Common.Abstractions;
+using Common.AbstractionsImplementations;
+using Common.Types;
+
+namespace Common.Tests.AbstractionsImplementations;
+
+public class ObserverTest
+{
+  #region Arrange
+
+  Common.Abstractions.IObserver<string> _observer;
+  IObserverListener<string> _listener => _observer;
+  IObserverNotifier<string> _notifier => _observer;
+
+  EventListener<string> _eventListener;
+  string _expected = "abc";
+  string? _notification;
+
+  public ObserverTest()
+  {
+    _observer = new Observer<string>();
+    _eventListener = new EventListener<string>(Id.New(), (notification) =>
+    {
+      _notification = notification;
+      return Task.CompletedTask;
+    });
+  }
+
+  #endregion Arrange
+
+  [Fact]
+  public async Task Lisner_Should_Be_Notified()
+  {
+    // Act
+    Res resSuccess = _listener.Subscribe(_eventListener);
+    await _notifier.Notify(_expected);
+
+    // Assert
+    Assert.True(resSuccess.IsSuccess, "subscribing should succed");
+    Assert.True(_notification is not null, "notification shouldn't be null");
+    Assert.True(_expected == _notification, "notification should be sent");
+  }
+
+  [Fact]
+  public async void Unsubscribing_Should_Work()
+  {
+    // Act
+    Res resSubscribe = _listener.Subscribe(_eventListener);
+    Res resUnsubscribe = _listener.Unsubscribe(_eventListener.Id);
+    await _notifier.Notify(_expected);
+
+    // Assert
+    Assert.True(resSubscribe.IsSuccess, "subscribing should succed");
+    Assert.True(resUnsubscribe.IsSuccess, "unsubscribing should succed");
+    Assert.True(_notification != _expected, "message shouldn't be handled by unsubscribed EventListener");
+  }
+
+  [Fact]
+  public void Subscribing_Many_Times_Should_Fail()
+  {
+    // Act
+    Res resSuccess = _listener.Subscribe(_eventListener);
+    Res resFailure = _listener.Subscribe(_eventListener);
+
+    // Assert
+    Assert.True(resSuccess.IsSuccess, "subscribing once should success");
+    Assert.True(resFailure.Exceptions.Count() == 1);
+  }
+
+  [Fact]
+  public void Unsubscribing_Should_Fail()
+  {
+    // Act
+    Res resFail = _listener.Unsubscribe(_eventListener.Id);
+
+    // Assert
+    Assert.True(resFail.IsFailure, "unsubscribing when not subscribed should fail");
+    Assert.True(resFail.Exceptions.Count() == 1, "unsubscribing when not subscribed should throw one error");
+  }
+}
---
+++ api/src/Common/Common.Tests/AbstractionsMocks/AppWebSocketMock.cs
@@ -0,0 +1,33 @@
+using Common.Abstractions;
+using Common.Types;
+
+namespace Common.Tests.AbstractionsMocks;
+
+public class AppWebSocketMock : IAppWebSocket
+{
+  Id _id;
+  Id IAppWebSocket.Id => _id;
+
+  bool _isOpen = true;
+  bool IAppWebSocket.IsOpen => _isOpen;
+
+  Common.Abstractions.IObserver<WebSocketMessage> _messageObserver;
+  IObserverListener<WebSocketMessage> IAppWebSocket.MessageListener => _messageObserver;
+
+  public AppWebSocketMock(Id id, Common.Abstractions.IObserver<WebSocketMessage> messageObserver)
+  {
+    _id = id;
+    _messageObserver = messageObserver;
+  }
+
+  public Task<Res> Close()
+  {
+    _isOpen = false;
+    return Task.FromResult(Res.Success());
+  }
+
+  public Task<Res> Send(WebSocketMessage message)
+  {
+    return Task.FromResult(Res.Success());
+  }
+}
