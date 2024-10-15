c9869aced572416ef3f128c700ebb4e854f61923
ogius <kowalewski.olgierd@gmail.com>
2024-10-15 19:35:19 +0200

    added empty Common.Core.Tests and Common.DAL.Tests

---
+++ api/src/Common/Common.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Common.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ api/src/Common/Common.DAL.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Common.DAL.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
